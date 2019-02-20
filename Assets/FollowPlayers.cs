22ff56013bcd53973147d8afdfb272306e2f947b
jtavitas <43662309+jtavitas@users.noreply.github.com>
2019-02-20 16:01:10 -0600

    InitProjectUpload

---
+++ Assets/FollowPlayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayers : MonoBehaviour {
+    public GameObject[] players;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3 (0,0,0);
+        foreach (GameObject player in players)
+        {
+            pos += player.transform.position;
+        }
+        pos = pos / players.Length;
+        transform.position = pos;
+	}
+}
---
+++ Assets/RotatePick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePick : MonoBehaviour {
+
+    public KeyCode keyLeft;
+    public KeyCode keyRight;
+    public float rotateSpeed = 1;
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(keyLeft))
+        {
+            //transform.Rotate(-rotateSpeed, 0, 0);
+            rb.AddTorque(0, 0, rotateSpeed);
+        }
+        if (Input.GetKey(keyRight))
+        {
+            //transform.Rotate(rotateSpeed, 0, 0);
+            rb.AddTorque(0, 0, -rotateSpeed);
+        }
+	}
+
+    
+}

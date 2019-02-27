10fc97dc102ed63a528217f32acf60fee95ecba2
joeyt <jktavitas@gmail.com>
2019-02-27 03:49:52 -0600

    PlayerText added
    Each player will show dialog every few seconds based on distance.

---
+++ Assets/Joe/Scripts/PlayerText.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerText : MonoBehaviour {
+	public float timer = 7.0f;
+	public bool timerRunning = true;
+	public bool showText = false;
+	public bool found = false;
+	public TextMesh tm;
+	public float dist;
+
+	PlayerMovement[] players;
+	// Use this for initialization
+	void Start () {
+		tm = GetComponent<TextMesh>();
+		players = GameObject.FindObjectsOfType<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timerRunning) {
+			timer -= Time.deltaTime;
+		}
+
+		if (timer <= 0.0f) {
+			if (!showText) {
+				timerEnded1 ();
+			} else {
+				timerEnded2 ();
+			}
+		}
+	}
+
+	void timerEnded1()
+	{
+		timer = 3.0f;
+		showText = true;
+		dist = Vector3.Distance(players[0].transform.position, players[1].transform.position);
+		dist = (1 / dist);
+		if (dist >= 0.75) {
+			tm.text = "Thanks for being my pardner,\n pardner.";
+			if (!found) found = true;
+		} else if (dist <= 0.3) {
+			tm.text = "It's lonely out here,\n pardner.";
+		} else {
+			if (!found) {
+				tm.text = "Wanna be my pardner,\n pardner?";
+			} else
+				tm.text = "Where'd ya go,\n pardner?";
+		}
+	}
+
+	void timerEnded2()
+	{
+		timer = 7.0f;
+		showText = false;
+		tm.text = "";
+	}
+}

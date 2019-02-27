50bfe95e4d77f10e16d13f0280a68997b1f0e1ba
joeyt <jktavitas@gmail.com>
2019-02-26 21:43:58 -0600

    BGM, Art, Level update

---
+++ Assets/Joe/Scripts/BGMFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMFade : MonoBehaviour {
+
+	public float volume = 0.5f;
+	AudioSource bgm;
+
+	PlayerMovement[] players;
+	// Use this for initialization
+	void Start () {
+		bgm = GetComponent<AudioSource> ();
+		bgm.volume = volume;
+		players = GameObject.FindObjectsOfType<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setVolume ();
+	}
+
+	void setVolume () {
+		float dist = Vector3.Distance(players[0].transform.position, players[1].transform.position);
+		volume = (1 / dist / 8);
+		bgm.volume = volume;
+	}
+}

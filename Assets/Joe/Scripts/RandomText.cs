18f7b622e170438dca9772b03d7f7a7032f723be
jtavitas <43662309+jtavitas@users.noreply.github.com>
2019-03-04 16:23:01 -0600

    RandomString script added

---
+++ Assets/Joe/Scripts/RandomText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomText : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_HardCodedStrings;
+
+    void Awake()
+    {
+        m_HardCodedStrings = new string[]
+            {
+                 "String Zero",
+                 "String One",
+                 "String Two",
+                 "String Three",
+                 "String Four",
+            };
+    }
+
+    void Update()
+    {
+        Debug.Log(GetRandomHardCodedString());
+    }
+
+    public string GetRandomHardCodedString()
+    {
+        return m_HardCodedStrings[Random.Range(0, m_HardCodedStrings.Length)];
+    }
+}
